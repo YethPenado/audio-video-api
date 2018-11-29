ac30f9a2b533580fcb96ab253a2b6cb24086e877
YethPenado <39468032+YethPenado@users.noreply.github.com>
2018-11-28 23:40:34 -0600

    Variables iniciales agregadas

--- av-api/src/js/script.js
+++ av-api/src/js/script.js
@@ -3,3 +3,16 @@
  * ...
  */
 
+/******
+
+const songs = [['../multimedia/Bohemian Rhapsody.mp3', 'Bohemian Rhapsody'], ['../multimedia/Killer Queen.mp3', 'Killer Queen'], ['../multimedia/Love of my Life.mp3', 'Love of my Life']];
+
+console.log(songs);
+
+******/
+
+const play = document.getElementById('play');
+const pause = document.getElementById('pause');
+const stop = document.getElementById('stop');
+const previous = document.getElementById('previous');
+const next = document.getElementById('next');
